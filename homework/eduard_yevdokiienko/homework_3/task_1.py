029fef5b5fb9bd1b8fc0b79818b7aa62acfd492e
Eduard Yevdokiienko <eduard.evdokienko@gmail.com>
2024-04-16 19:40:28 +0100

    homework_3 done

---
+++ homework/eduard_yevdokiienko/homework_3/task_1.py
@@ -0,0 +1,11 @@
+# Даны 2 числа a и b. Получить их сумму, разность и произведение
+a = 10
+b = 5
+
+sum_result = a + b
+difference_result = a -b
+product_result = a * b
+
+print('sum', sum_result)
+print('dif', difference_result)
+print('prod', product_result)
