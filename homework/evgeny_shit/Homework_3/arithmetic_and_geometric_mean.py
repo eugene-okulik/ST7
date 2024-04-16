ab47e143025afaed30a6a1594a3c9adb5c8483db
Dozer <shitov.ea@gmail.com>
2024-04-16 17:54:17 +0400

    Homework_3 done

---
+++ homework/evgeny_shit/Homework_3/arithmetic_and_geometric_mean.py
@@ -0,0 +1,10 @@
+# import math
+
+a = 20
+b = 5
+
+arithmetic_mean = (a + b) / 2
+geometric_mean = (a * b) ** 0.5
+# geometric_mean_math = math.sqrt(a * b)
+
+print(f"Arithmetic mean = {arithmetic_mean} and geometric mean = {geometric_mean}")
---
+++ homework/evgeny_shit/Homework_3/sum_difference_product.py
@@ -0,0 +1,8 @@
+a = 3
+b = 4
+
+sum = a + b
+
+print(f"The sum of {a} and {b} is {sum}")
+print(f"The difference of 3 and 4 = {a - b}")
+print(f"The product of 3 and 4 = {a * b}")
