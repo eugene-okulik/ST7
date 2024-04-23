6952376a4bdc4c58d16c0abac629ae3394611e9a
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2024-04-23 23:45:53 +0300

    sergey_zaichkov: added hw_4

---
+++ homework/sergey_zaichkov/hw_4/scheduler.py
@@ -0,0 +1,15 @@
+import math
+
+name = input("What is your name?\n")
+buy = input("What are you going to buy?\n")
+price = float(input("How much does it cost?\n"))
+current_money = float(input("How much money do you have?\n"))
+save_money = float(input("How much can you save per month?\n"))
+
+need_money = price - current_money
+make_purchase = current_money >= need_money
+months = math.ceil(need_money / save_money)
+
+print(f"Hello, {name}. You don't have enough ${need_money} to buy a {buy}\n"
+      f"Possibility of making a purchase: {make_purchase}\n"
+      f"{months} months left until purchase")
