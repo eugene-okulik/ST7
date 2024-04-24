5b740f4d52b26bf97e6cb9266610da669a427220
Dozer <shitov.ea@gmail.com>
2024-04-24 22:33:59 +0400

    task_1 - Removed unnecessary initializations and corrected quotes in f-strokes + minor adjustments
    task_2 - Added recalculate function to minimize repetitions

--- homework/evgeny_shit/Homework_5/task_1-dictionary.py
+++ homework/evgeny_shit/Homework_5/task_1-dictionary.py
@@ -3,32 +3,30 @@ import random
 my_dict = {'tuple': (1, 2, 3, 4, 5),
            'dict': {'first': 11, 'second': 22, 'third': 33, 'fourth': 44, 'fifth': 55},
            'list': [10, 20, 30, 40, 50],
-           'set': {'one', 'two', 'three', 'four', 'five'}}
+           'set': {'one', 'two', 'three', 'four', 'five'}
+           }
 
 #  Tuple action.
-last_element = my_dict['tuple'][-1]
-print(f'The last element of the tuple \n{last_element}\n')
+print(f"The last element of the tuple \n{my_dict['tuple'][-1]}\n")
 
 #  List action
 my_dict['list'].append(60)
 my_dict['list'].pop(1)
-dict_list = my_dict['list']
 
-print(f'Adding the value "60" to the list and deleting the 2nd item\n{dict_list}\n')
+print(f"Adding the value '60' to the list and deleting the 2nd item\n{my_dict['list']}\n")
 
 #  Dict action
 my_dict['dict'][('i am a tuple',)] = (111,)
 random_key = random.choice(list(my_dict['dict'].keys()))
 del my_dict['dict'][random_key]
-dict_dict = my_dict['dict']
 
-print(f'Adding a key ("i am a tuple,") with the value "(111, )" and deleting a random element \n{dict_dict}\n')
+print(f"Adding a key ('i am a tuple,') with the value '(111,)' and deleting a random element \n{my_dict['dict']}\n")
 
 #  Set action
 my_dict['set'].add('six')
 my_dict['set'].pop()
 dict_set = my_dict['set']
 
-print(f'Adding the value "six" and deleting the value "three"\n{dict_set}\n')
+print(f"Adding the value 'six' and deleting the value 'three'\n{my_dict['set']}\n")
 
 print(f'The entire dictionary "my_dict" \n{my_dict}')
--- homework/evgeny_shit/Homework_5/task_2-result_recalculation.py
+++ homework/evgeny_shit/Homework_5/task_2-result_recalculation.py
@@ -1,13 +1,14 @@
+def recalculate(s):
+    number = int(s[s.find(":") + 2:])
+    return number + 10
+
+
 str1 = 'результат операции: 42'
 str2 = 'результат операции: 514'
 str3 = 'результат работы программы: 9'
 
-num1 = int(str1[str1.find(":") + 2:])
-num2 = int(str2[str2.find(":") + 2:])
-num3 = int(str3[str3.find(":") + 2:])
-
-result1 = num1 + 10
-result2 = num2 + 10
-result3 = num3 + 10
+result1 = recalculate(str1)
+result2 = recalculate(str2)
+result3 = recalculate(str3)
 
 print(result1, result2, result3)
