fce9d7047c4ddc2b63582502c9167db23038d5e0
Alex Verteyko <cracowaqa@gmail.com>
2024-04-24 20:33:11 +0200

    add task5.2.py

---
+++ homework/alex_v/homework5/task5.2.py
@@ -0,0 +1,7 @@
+operation_one = "результат операции: 42"
+operation_two = "результат операции: 514"
+operation_three = "результат операции: 9"
+
+print(int(operation_one[-2:]) + 10)
+print(int(operation_two[-3:]) + 10)
+print(int(operation_three[-2:]) + 10)
