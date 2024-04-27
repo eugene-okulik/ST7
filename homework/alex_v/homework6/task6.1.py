f3cfc08505a8c2bb9c166047b98c6c7294de0221
Alex Verteyko <cracowaqa@gmail.com>
2024-04-27 21:29:39 +0200

    add task6.1.py

---
+++ homework/alex_v/homework6/task6.1.py
@@ -0,0 +1,14 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+
+split_words = text.split()
+print(split_words)
+
+for word in split_words:
+    special_signs = ''
+    if word[-1] in [',', '.']:
+        special_signs = split_words[-1]
+        word = word[:-1]
+    result = [word + 'ing' + special_signs]
+    new_text = ''.join(result)
+
+    print(new_text)
