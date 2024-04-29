87180b3a65ee079f3496e55b591932360a73c6a1
Alex Verteyko <cracowaqa@gmail.com>
2024-04-29 22:30:05 +0200

    refactor 5.2 task

--- homework/alex_v/homework5/task5.2.py
+++ homework/alex_v/homework5/task5.2.py
@@ -2,6 +2,10 @@ operation_one = "результат операции: 42"
 operation_two = "результат операции: 514"
 operation_three = "результат операции: 9"
 
-print(int(operation_one[-2:]) + 10)
-print(int(operation_two[-3:]) + 10)
-print(int(operation_three[-2:]) + 10)
+result1 = int(operation_one.split(': ')[-1]) + 10
+result2 = int(operation_two.split(': ')[-1]) + 10
+result3 = int(operation_three.split(': ')[-1]) + 10
+
+print(result1)
+print(result2)
+print(result3)
