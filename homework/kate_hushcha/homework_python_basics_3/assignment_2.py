56816406e92e2fd31c8264801bf5ba31cf8bf6bc
Kate Hushcha <katerinahushcha@gmail.com>
2024-05-01 18:26:03 -0400

    tried to fix the assignments

--- homework/kate_hushcha/homework_python_basics_3/assignment_2.py
+++ homework/kate_hushcha/homework_python_basics_3/assignment_2.py
@@ -3,19 +3,17 @@ line_1 = 'результат операции: 42'
 line_2 = 'результат операции: 514'
 line_3 = 'результат работы программы: 9'
 
+add_line_1 = line_1.split()
+plus_number_1 = add_line_1[-1]
+summ_1 = int(plus_number_1) + 10
+print(summ_1)
 
-list_1 = line_1.split()
-list_2 = line_2.split()
-list_3 = line_3.split()
+add_line_2 = line_2.split()
+plus_number_2 = add_line_2[-1]
+summ_2 = int(plus_number_2) + 10
+print(summ_2)
 
-number_1 = int(list_1[-1])
-total_1 = number_1 + 10
-print(total_1)
-
-number_2 = int(list_2[-1])
-total_2 = number_2 + 10
-print(total_2)
-
-number_3 = int(list_3[-1])
-total_3 = number_3 + 10
-print(total_3)
+add_line_3 = line_3.split()
+plus_number_3 = add_line_3[-1]
+summ_3 = int(plus_number_3) + 10
+print(summ_3)
