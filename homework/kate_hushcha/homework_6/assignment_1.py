552d4fd36f881780198d3ed03de9c14d190632ac
Kate Hushcha <katerinahushcha@gmail.com>
2024-05-01 06:49:52 -0400

    fixed the lines

--- homework/kate_hushcha/homework_6/assignment_1.py
+++ homework/kate_hushcha/homework_6/assignment_1.py
@@ -1,7 +1,6 @@
 
 text_1 = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.'
-'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
-)
+          'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 
 list_text_1 = text_1.split()
 for words in list_text_1:
