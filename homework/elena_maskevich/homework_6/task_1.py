4efda5ca846d6b3c3a42d39a621c5f290100c2bd
ElenaMaskeivh <maskevich1993@mail.ru>
2024-05-01 07:04:01 +0300

    fixed task1

--- homework/elena_maskevich/homework_6/task_1.py
+++ homework/elena_maskevich/homework_6/task_1.py
@@ -4,7 +4,12 @@ list_from_text = text.split()
 print(list_from_text)
 new_list = []
 for world in list_from_text:
-    new_list.append('ing' + world)
+    if world.endswith(','):
+        new_list.append(world[:-1] + 'ing' + ',')
+    elif world.endswith('.'):
+        new_list.append(world[:-1] + 'ing' + '.')
+    else:
+        new_list.append(world + 'ing')
 print(new_list)
 updated_text = ' '.join(new_list)
 print(updated_text)
