5271f72dddbc6316480b0bcc5160641ec259fffe
Kate Hushcha <katerinahushcha@gmail.com>
2024-05-02 19:10:09 -0400

    fixed assignment 2

--- homework/kate_hushcha/homework_python_basics_3/assignment_2.py
+++ homework/kate_hushcha/homework_python_basics_3/assignment_2.py
@@ -3,17 +3,17 @@ line_1 = 'результат операции: 42'
 line_2 = 'результат операции: 514'
 line_3 = 'результат работы программы: 9'
 
-add_line_1 = line_1.split()
-plus_number_1 = add_line_1[-1]
-summ_1 = int(plus_number_1) + 10
-print(summ_1)
+pos_1 = line_1.find(':')
+sum_1 = pos_1+2
+number_1 = int(line_1[sum_1:]) + 10
+print(number_1)
 
-add_line_2 = line_2.split()
-plus_number_2 = add_line_2[-1]
-summ_2 = int(plus_number_2) + 10
-print(summ_2)
+pos_2 = line_2.find(':')
+sum_2 = pos_2+2
+number_2 = int(line_2[sum_2:]) + 10
+print(number_2)
 
-add_line_3 = line_3.split()
-plus_number_3 = add_line_3[-1]
-summ_3 = int(plus_number_3) + 10
-print(summ_3)
+pos_3 = line_3.find(':')
+sum_3 = pos_3+2
+number_3 = int(line_3[sum_3:]) + 10
+print(number_3)
