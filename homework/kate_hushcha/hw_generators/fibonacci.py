af97eed1b1343226d2a1c47cd4c40783c7f11f06
Kate Hushcha <katerinahushcha@gmail.com>
2024-05-08 20:07:39 -0400

    hw_fibonacci_completed

---
+++ homework/kate_hushcha/hw_generators/fibonacci.py
@@ -0,0 +1,23 @@
+
+def fibonacci():
+    first_num = 0
+    second_num = 1
+    while True:
+        yield first_num
+        first_num = second_num
+        second_num = first_num + second_num
+
+                            
+count = 0
+for num in fibonacci():
+    if count == 4:
+        print(num)
+    count += 1
+    if count == 199:
+        print(num)
+    if count == 999:
+        print(num)
+    if count == 99999:
+        print(num)
+        break
+
