86b0d2070ea1b5c23b24f0e48a48e42a8498be18
Kate Hushcha <katerinahushcha@gmail.com>
2024-05-10 07:07:22 -0400

    corrected mistakes

--- homework/kate_hushcha/hw_generators/fibonacci.py
+++ homework/kate_hushcha/hw_generators/fibonacci.py
@@ -4,15 +4,15 @@ def fibonacci():
     second_num = 1
     while True:
         yield first_num
+        result = first_num + second_num
         first_num = second_num
-        second_num = first_num + second_num
+        second_num = result
 
 
 count = 0
 for num in fibonacci():
     if count == 4:
         print(num)
-    count += 1
     if count == 199:
         print(num)
     if count == 999:
@@ -20,3 +20,4 @@ for num in fibonacci():
     if count == 99999:
         print(num)
         break
+    count += 1
