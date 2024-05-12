c01cb57d76f4b9480d6fc0478e8d321a0fef79bb
Viktor1-lab <putin975@yandex.ru>
2024-05-12 10:38:00 +0300

    fixed, step 3

--- homework/TumanovViktor/hw5/tumanov_hw5.py
+++ homework/TumanovViktor/hw5/tumanov_hw5.py
@@ -4,8 +4,7 @@ my_dict['my_list'] = [1, 3, 6, 7, 'text', False, 'last', 'sdfsdf']
 my_dict['my_list'].append(12)  # Добавление элемента
 my_dict['my_list'].pop(1)  # Удаление второго элемента по индексу.
 my_dict['dict'] = {'one': 'value1', 'two': 'value2', 'free': 'value3', 'four': 'value4', 'fife': 'value5'}
-dicta = {'i am a tuple': 'Tumanov'}
-my_dict['dict'].update(dicta)  # Добавление через соединения словарей.
+my_dict['dict'][('i am a tuple',)] = 'tumanov_done'
 my_dict['dict'].pop('one')  # Удаление элемента по ключ-значение. Так же можно удалить через del
 my_dict['my_set'] = {1, 3, 6, 7, 'task', 'text', 'False', 3}
 my_dict['my_set'].add('Новый элемент')  # добовляет элемент во множество
