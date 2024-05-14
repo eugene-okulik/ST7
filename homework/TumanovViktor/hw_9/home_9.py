10a11eed273645abee3f656422b75690571a02de
Viktor1-lab <putin975@yandex.ru>
2024-05-14 15:22:55 +0300

    fixed 2

--- homework/TumanovViktor/hw_9/home_9.py
+++ homework/TumanovViktor/hw_9/home_9.py
@@ -1,11 +1,12 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
                 30, 28, 24, 23]
 
-max_temp = list(filter(lambda x: x == max(temperatures), temperatures))
-min_temp = list(filter(lambda x: x == min(temperatures), temperatures))
 jar_temp = list(filter(lambda x: x >= 28, temperatures))
-average = sum(temperatures) / len(temperatures)
-print('максимальная температура', list(set(max_temp)),
+max_temp = list(filter(lambda x: x == max(jar_temp), jar_temp))
+min_temp = list(filter(lambda x: x == min(jar_temp), jar_temp))
+average = sum(jar_temp) / len(jar_temp)
+
+print('Жаркие дни', jar_temp,
       '\n' 'минимальная температура', min_temp,
-      '\n' 'Жаркие дни', jar_temp,
+      '\n' 'максимальная температура', list(set(max_temp)),
       '\n', round(average))
