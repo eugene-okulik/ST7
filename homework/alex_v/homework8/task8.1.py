b72486c6c659300797d995d0e18840112e4ce9be
Alex Verteyko <cracowaqa@gmail.com>
2024-05-14 14:59:26 +0200

    refactor 8.1 task

--- homework/alex_v/homework8/task8.1.py
+++ homework/alex_v/homework8/task8.1.py
@@ -5,27 +5,3 @@ def fibonacci_generator():
         a, b = b, a + b
 
 fibonacci = fibonacci_generator()
-
-# Получение пятого числа Фибоначчи
-fifth_number = next(fibonacci)
-for _ in range(4):
-    fifth_number = next(fibonacci)
-
-print("Fifth number in Fibonacci sequence:", fifth_number)
-
-# Получение 200-го числа Фибоначчи
-for _ in range(195):  # Итерируем 200 - 5 раз, так как уже получили пятое число
-    next(fibonacci)
-two_hundredth_number = next(fibonacci)
-print("Two hundredth number in Fibonacci sequence:", two_hundredth_number)
-
-# Получение 1000-го числа Фибоначчи
-for _ in range(795):  # Итерируем 1000 - 200 - 5 раз
-    next(fibonacci)
-thousandth_number = next(fibonacci)
-print("Thousandth number in Fibonacci sequence:", thousandth_number)
-
-# Получение 100000-го числа Фибоначчи
-for _ in range(99795):  # Итерируем 100000 - 1000 - 200 - 5 раз
-    next(fibonacci)
-hundred_thousandth_number = next(fibonacci)
