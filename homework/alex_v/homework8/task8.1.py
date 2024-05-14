fda02995daa9bad36d695b39e76ed6367e57e639
Alex Verteyko <cracowaqa@gmail.com>
2024-05-15 00:07:56 +0200

    refactor 8.1 task

--- homework/alex_v/homework8/task8.1.py
+++ homework/alex_v/homework8/task8.1.py
@@ -4,4 +4,13 @@ def fibonacci_generator():
         yield a
         a, b = b, a + b
 
+
 fibonacci = fibonacci_generator()
+
+count = 0
+
+for num in fibonacci_generator():
+    print(num) if count in [4, 199, 999, 99999] else None
+    count = count + 1
+    if count > 99999:
+        break
