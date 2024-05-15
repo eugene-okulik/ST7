4341487736935313245c29fe665759953414bb45
Dmitrii Kiselev <didroi@yandex.ru>
2024-05-15 19:11:28 +0200

    homework_8_correction

--- homework/dmitrii_kiselev/homework_8/task_1.py
+++ homework/dmitrii_kiselev/homework_8/task_1.py
@@ -1,13 +1,11 @@
 def fibonacci():
-    fibonacci_number_previous, fibonacci_number_pre_previous = 0, 1
+    fibonacci_number, fibonacci_number_next = 0, 1
     while True:
-        fibonacci_number = fibonacci_number_previous + fibonacci_number_pre_previous
         yield fibonacci_number
-        fibonacci_number_pre_previous = fibonacci_number_previous
-        fibonacci_number_previous = fibonacci_number
+        fibonacci_number, fibonacci_number_next = fibonacci_number_next, fibonacci_number + fibonacci_number_next
 
 
-result_list, count = [5, 200, 1000, 100000], 2
+result_list, count = [5, 200, 1000, 100000], 1
 
 for x in fibonacci():
     if count in result_list:
