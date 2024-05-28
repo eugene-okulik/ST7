7c8cb14fec7fecb60f65dcc15773fd1846a43bab
Eugeny Okulik <tiburon854@gmail.com>
2024-05-28 12:53:02 +0300

    files from lesson 13

---
+++ homework/eugene_okulik/Lesson_13/test.py
@@ -0,0 +1,26 @@
+import json
+
+
+class CountryData:
+    def __init__(self, filename):
+        self.__filename = filename
+        self.__data = self.__read_file()
+        self.__country = self.__data['Country']
+        self.__avg_temp = self.__data['avg_temp']
+        self._is_hot = True if self.__avg_temp >= 30 else False
+
+    @property
+    def data(self):
+        return self.__data
+
+    def __read_file(self):
+        file_data = open(self.__filename)
+        data = json.load(file_data)
+        return data
+
+
+
+data1 = CountryData('data1.txt')
+
+print(data1.data)
+print(data1._is_hot)
