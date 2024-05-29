75567caf2cf6a3bd61ed3dd22253886248dfb5f6
Viktor1-lab <putin975@yandex.ru>
2024-05-29 18:00:50 +0300

    Tumanov 13 done

---
+++ homework/TumanovViktor/hw_13/home_13.py
@@ -0,0 +1,46 @@
+class Flowrs:
+    def __init__(self, name, color, svezhest, lifespan, price, srok_zhizni):
+        self.name = name
+        self.color = color
+        self.svezhest = svezhest
+        self.lifespan = lifespan
+        self.price = price
+        self.srok_zhizni = srok_zhizni
+
+
+class Buket:
+    def __init__(self):
+        self.tsvety = []
+
+    def add_flower(self, tsvetok):
+        self.tsvety.append(tsvetok)
+
+    def lifespan(self):
+        lifespan = sum(tsvetok.srok_zhizni for tsvetok in self.tsvety)
+        return lifespan / len(self.tsvety) if len(self.tsvety) > 0 else 0
+
+    def sort(self, key):
+        self.tsvety.sort(key=lambda x: getattr(x, key))
+
+    def search(self, key, value):
+        return [tsvetok for tsvetok in self.tsvety if getattr(tsvetok, key) == value]
+
+
+roza = Flowrs("Роза", "Красный", 77, 24, 150, 10)
+arhideya = Flowrs("Архидея", "Фиолетовый", 88, 20, 80, 7)
+romashka = Flowrs("Ромашка", "Белый с жёлтый", 99, 12, 55, 6)
+fialki = Flowrs("Фиалки", "Синий", 450, 756, 255, 15)
+
+
+buket = Buket()
+buket.add_flower(roza)
+buket.add_flower(arhideya)
+buket.add_flower(romashka)
+buket.add_flower(fialki)
+
+
+print("Среднее время увядания букета:", int(buket.lifespan()))
+buket.sort("price")
+print("Цветы в букете отсортированы по цене:", [tsvetok.name for tsvetok in buket.tsvety])
+yellow_color = buket.search("color", "Синий")
+print("Цветы в букете желтого цвета:", [tsvetok.name for tsvetok in yellow_color])
