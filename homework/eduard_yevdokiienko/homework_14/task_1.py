638337c650b193197cdff201cd64208f7a500081
Eduard Yevdokiienko <eduard.evdokienko@gmail.com>
2024-06-04 13:53:34 +0100

    eduard:homework_14 done

---
+++ homework/eduard_yevdokiienko/homework_14/task_1.py
@@ -0,0 +1,16 @@
+import datetime
+
+date_format = '%d/%m/%Y'
+
+while True:
+    user_input = input('Please type your birth date: ')
+
+    try:
+        birth_date = datetime.datetime.strptime(user_input, date_format)
+        current_date = datetime.datetime.now()
+        age_in_days = (current_date - birth_date).days
+        print(f'Age in days: {age_in_days}')
+        break
+    except ValueError:
+        print('Invalid date format. Please enter the date in the format: dd/mm/yyyy')
+        
---
+++ homework/eduard_yevdokiienko/homework_14/task_2.py
@@ -0,0 +1,10 @@
+import os
+
+dir_path = 'homework/eugene_okulik/hw_13/'
+file_path = os.path.join(dir_path, 'data.txt')
+
+with open(file_path, 'r', encoding='utf-8') as my_file:
+    data = my_file.read()
+    for upper_word in data:
+        if upper_word.isupper():
+            print(upper_word, end='')
