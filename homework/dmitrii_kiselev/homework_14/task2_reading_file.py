d7afe5a399352356076ce3ec55851b55d655190f
Dmitrii Kiselev <didroi@yandex.ru>
2024-06-08 20:06:57 +0200

    dmitrii_hw14_done

--- homework/dmitrii_kiselev/homework_14/task2_reading_file.py
+++ homework/dmitrii_kiselev/homework_14/task2_reading_file.py
@@ -3,8 +3,8 @@ import os
 
 sep = '/' if sys.platform in ['linux', 'darwin'] else '\\'
 
-# к сожалению, не нашел лучшего решения, пришлось как все сисывать это решение))
-# протестировать на линукс или маке не смог
+#  к сожалению, не нашел лучшего решения, пришлось как все сисывать это решение))
+#  протестировать на линукс или маке не смог
 url_begining = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 with open(f'{url_begining}{sep}eugene_okulik{sep}hw_13{sep}data.txt', 'r', encoding='utf-8') as eugenes_file:
