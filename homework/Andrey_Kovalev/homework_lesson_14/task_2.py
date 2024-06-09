f348fe34eb0424337580d942d8de3bbd02b17769
kovandre0 <kovandre136@gmail.com>
2024-06-09 12:00:48 +0300

    исправление

--- homework/Andrey_Kovalev/homework_lesson_14/task_2.py
+++ homework/Andrey_Kovalev/homework_lesson_14/task_2.py
@@ -1,5 +1,12 @@
-with open('D:/testST7/ST7/homework/eugene_okulik/hw_13/data.txt') as file:
-    for i in file:
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+file_path = os.path.join(project_root, 'homework', 'eugene_okulik', 'hw_13')
+eugene_okulik = os.path.join(file_path, 'data.txt')
+
+
+with open(eugene_okulik, encoding='utf-8') as my_file:
+    for i in my_file:
         for char in i:
             if char.isupper():
                 print(char, end='')
