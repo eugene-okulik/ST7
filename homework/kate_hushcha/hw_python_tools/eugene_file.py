8bc7f92defa9dc97970291bab9b05ddad63e3763
Kate Hushcha <katerinahushcha@gmail.com>
2024-06-10 20:49:43 -0400

    fixed_the_path

--- homework/kate_hushcha/hw_python_tools/eugene_file.py
+++ homework/kate_hushcha/hw_python_tools/eugene_file.py
@@ -1,6 +1,9 @@
 import os
 
-file_path = 'C:\\Users\\kater\\ST7\\homework\\eugene_okulik\\hw_13\\data.txt'
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+eugene_path = os.path.join(root_path, 'homework', 'eugene_okulik', 'hw_13')
+file_path = os.path.join(eugene_path, 'data.txt')
 
 with open(file_path, 'r', encoding='utf-8') as my_file:
     data = my_file.read()
