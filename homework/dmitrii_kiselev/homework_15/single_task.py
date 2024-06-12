167a2ab8e888a28986402eecd05e244fd7908ddb
Dmitrii Kiselev <didroi@yandex.ru>
2024-06-12 20:08:03 +0200

    reunion

--- homework/dmitrii_kiselev/homework_15/single_task.py
+++ homework/dmitrii_kiselev/homework_15/single_task.py
@@ -1,15 +1,10 @@
-import sys
 import os
 import datetime
 
-sep = '/' if sys.platform in ['linux', 'darwin'] else '\\'
-
 url_begining = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-with open(f'{url_begining}{sep}eugene_okulik{sep}hw_15{sep}data.txt', 'r', encoding='utf-8') as eugenes_file:
+with open(os.path.join(url_begining, 'eugene_okulik', 'hw_15', 'data.txt'), 'r', encoding='utf-8') as eugenes_file:
     data = eugenes_file.readlines()
-
-print(data)
 
 date = datetime.datetime.strptime(data[0][3:29], '%Y-%m-%d %H:%M:%S.%f')
 print(date + datetime.timedelta(days=7))
