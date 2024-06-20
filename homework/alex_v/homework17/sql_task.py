f96da276edc9d7d846461a55a08594e42148ca4f
Alex Verteyko <cracowaqa@gmail.com>
2024-06-20 22:35:55 +0300

    home task 17

--- homework/alex_v/homework17/sql_task.py
+++ homework/alex_v/homework17/sql_task.py
@@ -42,3 +42,64 @@ with mysql.connect(
         subject2 = cursor.lastrowid
         db.commit()
 
+
+    def create_lessons():
+        global ids
+        ids = []
+        create_lessons = "INSERT INTO lessons (title, subject_id) values (%s,%s)"
+
+        for lesson in [('Theory of economics', subject1),
+                       ('Macro ecnomics', subject1),
+                       ('Reading', subject2),
+                       'Writing', subject2]:
+            cursor.execute(create_lessons, lesson)
+        db.commit()
+
+
+    def set_marks():
+        set_marks = 'INSERT INTO marks (value, lesson_id,student_id) values (%s,%s,%s)'
+
+        for marks in [(7, ids[0], student_id),
+                      (8, ids[1], student_id),
+                      (7, ids[2], student_id),
+                      (2, ids[3], student_id)]:
+            cursor.execute(set_marks, marks)
+            db.commit()
+
+
+    def get_student_marks():
+        get_marks = 'SELECT value from marks where student_id = %s'
+        cursor.execute(get_marks, (student_id,))
+        marks = cursor.fetchall()
+        for m in marks:
+            print(m['value'])
+
+
+    def get_student_books():
+        get_books = (
+            "SELECT title FROM books join students on books.taken_by_student_id = students.id "
+            "where students.id = %s")
+
+        cursor.execute(get_books, (student_id,))
+        cursor.fetchall()
+
+
+    def get_full_info_about_student():
+        get_all = '''
+        select * from students join books on students.id= books.taken_by_student_id
+        join 'groups' on students.group_id = group.id
+        join marks on students.id = marks.student_id join lessons 
+        on marks.lesson_id = lessons.id join subjects on lessons.subjects.id where.id=%s
+        '''
+        cursor.execute(get_all, (student_id,))
+        cursor.fetchall()
+
+create_subjects()
+create_and_assign_books_to_students()
+create_students_group()
+create_subjects()
+create_lessons()
+set_marks()
+get_student_marks()
+get_student_books()
+get_full_info_about_student()
