b4cd0fc06800ab884123073d945314987245c310
ElenaMaskeivh <maskevich1993@mail.ru>
2024-06-22 20:35:56 +0300

    fixed linter

--- homework/elena_maskevich/homework_20/task_1.py
+++ homework/elena_maskevich/homework_20/task_1.py
@@ -16,10 +16,10 @@ def create_object():
     payload = {
         "name": "Apple MacBook Pro 16",
         "data": {
-          "year": 2019,
-          "price": 1849.99,
-          "CPU model": "Intel Core i9",
-          "Hard disk size": "1 TB"
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
         }
     }
     headers = {
@@ -48,11 +48,11 @@ def update_with_put(obj_id):
     payload = {
         "name": "Apple MacBook Pro 16",
         "data": {
-          "year": 2019,
-          "price": 2049.99,
-          "CPU model": "Intel Core i9",
-          "Hard disk size": "1 TB",
-          "color": "silver"
+            "year": 2019,
+            "price": 2049.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB",
+            "color": "silver"
         }
     }
 
