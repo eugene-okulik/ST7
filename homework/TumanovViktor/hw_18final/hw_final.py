95910cc2c15c6d70e556ab2bccd869bbe0fd0e39
Viktor1-lab <putin975@yandex.ru>
2024-06-23 12:05:18 +0300

    fixed final last fix

--- homework/TumanovViktor/hw_18final/hw_final.py
+++ homework/TumanovViktor/hw_18final/hw_final.py
@@ -1,25 +1,27 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file_path", help='D:/Python/projectTumanov/st7/homework/eugene_okulik/data/logs/')
+
+parser = argparse.ArgumentParser(description="text search")
+parser.add_argument("file_path", help="enter file path")
 parser.add_argument("--text", help="WARN", required=True)
 
 args = parser.parse_args()
 
-if os.path.exists(args.file_path):
+if os.path.exists(args.file_path) and os.path.isfile(args.file_path):
     with open(args.file_path, encoding='utf-8') as file:
         found = False
         for line_number, line in enumerate(file, start=1):
-            words = line.split()
-            if args.text in words:
+            if args.text in line:
                 found = True
-                word_index = words.index(args.text)
-                start_index = max(0, word_index - 5)
-                end_index = min(len(words), word_index + 6)
-                context = ' '.join(words[start_index:end_index])
-                print(f"{line_number}: {context}")
+                words = line.split()
+                indices = [i for i, word in enumerate(words) if args.text in word]
+                for word_index in indices:
+                    start_index = max(0, word_index - 5)
+                    end_index = min(len(words), word_index + 6)
+                    context = ' '.join(words[start_index:end_index])
+                    print(f"{line_number}: {context} в файле {args.file_path}")
         if not found:
-            print('Слово не найдено')
+            print('Текст не найден в файле.')
 else:
-    print(f"Файла {args.file_path} Не существует.")
+    print(f"Файл {args.file_path} не существует.")
