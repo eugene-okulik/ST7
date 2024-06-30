6a549b4905ad756c82cb5a73b1dfe603205871e8
Dmitrii Kiselev <didroi@yandex.ru>
2024-06-30 18:19:51 +0200

    21

--- homework/dmitrii_kiselev/homework_21/test_api.py
+++ homework/dmitrii_kiselev/homework_21/test_api.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.fixture()
-def create_object():
+def object_id():
     print('Start testing')
     payload = {
         "name": "Asus Ultrabook 2",
@@ -32,14 +32,14 @@ def create_object():
 
 
 @pytest.fixture()
-def follow_the_testing():
+def follow_the_testing_without_object():
     print('Start testing')
     yield
     print('Testing completed')
 
 
 @pytest.mark.regression
-def test_create_object(follow_the_testing):
+def test_create_object(follow_the_testing_without_object):
 
     payload = {
         "name": "Asus Ultrabook 2",
@@ -65,24 +65,56 @@ def test_create_object(follow_the_testing):
 
 
 @pytest.mark.regression
+def test_delete_object(follow_the_testing_without_object):
+    payload = {
+        "name": "Asus Ultrabook 2",
+        "data": {
+            "year": 2021,
+            "price": 837.99,
+            "CPU model": "Intel Core i7",
+            "Hard disk size": "1 TB"
+        }
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(
+        'https://api.restful-api.dev/objects',
+        json=payload,
+        headers=headers
+    )
+
+    obj_id = response.json()['id']
+
+    response = requests.delete(
+        f'https://api.restful-api.dev/objects/{obj_id}',
+        json=payload,
+        headers=headers
+    )
+
+    assert response.status_code == 200
+
+
+@pytest.mark.regression
 @pytest.mark.smoke
-def test_get_object(create_object):
+def test_get_object(object_id):
     headers = {
         'Content-Type': 'application/json'
     }
 
     response = requests.get(
-        f'https://api.restful-api.dev/objects/{create_object}',
+        f'https://api.restful-api.dev/objects/{object_id}',
         headers=headers
     )
 
     assert response.status_code == 200
-    print('id = ', create_object)
 
 
 @pytest.mark.regression
 @pytest.mark.critical
-def test_update_object(create_object):
+def test_update_object(object_id):
 
     payload = {
         "name": "Asus Ultrabook 2a",
@@ -99,18 +131,17 @@ def test_update_object(create_object):
     }
 
     response = requests.put(
-        f'https://api.restful-api.dev/objects/{create_object}',
+        f'https://api.restful-api.dev/objects/{object_id}',
         json=payload,
         headers=headers
     )
 
     assert response.status_code == 200
     assert response.json()['name'] == payload["name"]
-    print('id = ', create_object)
 
 
 @pytest.mark.regression
-def test_partually_update_object(create_object):
+def test_partually_update_object(object_id):
 
     payload = {
         "data": {
@@ -124,7 +155,7 @@ def test_partually_update_object(create_object):
     }
 
     response = requests.patch(
-        f'https://api.restful-api.dev/objects/{create_object}',
+        f'https://api.restful-api.dev/objects/{object_id}',
         json=payload,
         headers=headers
     )
@@ -132,4 +163,3 @@ def test_partually_update_object(create_object):
     assert response.status_code == 200
     assert response.json()['data']['Hard disk size'] == payload['data']['Hard disk size']
     assert response.json()['data']['price'] == payload['data']['price']
-    print('id = ', create_object)
