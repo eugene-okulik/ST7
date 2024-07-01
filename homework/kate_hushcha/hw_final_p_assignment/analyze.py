7b1e7c20f8798f3bddb7f96c2a69783400c883eb
Kate Hushcha <katerinahushcha@gmail.com>
2024-07-01 12:08:49 -0400

    fixed_folder_and_others

--- homework/kate_hushcha/hw_final_p_assignment/analyze.py
+++ homework/kate_hushcha/hw_final_p_assignment/analyze.py
@@ -1,25 +1,36 @@
 import argparse
-
+import os
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument("file_path", help="Your file path")
+parser.add_argument("path", help="Your file path")
 parser.add_argument("--word", help="The word")
 parser.add_argument("--valid", help="search for date", action="store_true")
-
 args = parser.parse_args()
 
-with open(args.file_path, encoding='utf-8') as file:
-    user_file = file.read()
 
-file_lines = user_file.split('\n')
+def a_file(file_path, search_word):
+    try:
+        with open(file_path, encoding='utf-8') as file:
+            user_file = file.read()
+        file_lines = user_file.split('\n')
+        for line_number, line in enumerate(file_lines, start=1):
+           if search_word in line:
+                words_count = line.split()
+                w_index = words_count.index(args.word)
+                before_w = w_index - 5
+                after_w = w_index + 5
+                print(f'File {file_path}, line: {line_number}, words: {words_count[before_w], words_count[w_index], words_count[after_w]}')
+    except:
+        if not search_word in line:
+            print('Word not found')
 
-for line_number, line in enumerate(file_lines, start=1):
-    if args.word in line:
-        words_count = line.split()
-        w_index = words_count.index(args.word)
-        before_w = w_index - 5
-        after_w = w_index + 5
-        print(words_count[before_w], words_count[w_index], words_count[after_w])
-    else:
-        print('Word is not found')
+
+if os.path.isfile(args.path):
+    a_file(args.path, args.word)
+elif os.path.isdir(args.path):
+    for file_name in os.listdir(args.path):
+        file_path = os.path.join(args.path, file_name)
+        if os.path.isfile(file_path):
+            a_file(file_path, args.word)
+else:
+    print('The path is wrong')
