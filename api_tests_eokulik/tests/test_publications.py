6eedb716cb8e46c21f02b7fd3692e254cdb12c63
Eugeny Okulik <tiburon854@gmail.com>
2024-07-11 19:50:30 +0300

    Files from lesson 25 - deletion fixture

--- api_tests_eokulik/tests/test_publications.py
+++ api_tests_eokulik/tests/test_publications.py
@@ -1,13 +1,17 @@
 import allure
 from api_tests_eokulik.tests.data import payloads
 from api_tests_eokulik.enpoints.post_posts import PostPosts
+from api_tests_eokulik.utils.delete_pub import del_pub
 
 
-def test_publication_create():
+def test_publication_create(delete_after_test):
     create_pub_endpoint = PostPosts()
-    create_pub_endpoint.create_publication(payloads.new_pub)
+    delete_after_test.obj_id = create_pub_endpoint.create_publication(payloads.new_pub)
+    # delete_publication.obj_id = create_pub_endpoint.obj_id
+    # obj_id = create_pub_endpoint.obj_id
     assert create_pub_endpoint.check_status_code_is_(201)
     create_pub_endpoint.check_response_title_is_(payloads.new_pub['title'])
+    # del_pub(obj_id)
 
 
 @allure.description('This test checks getting a publication by its Id')
