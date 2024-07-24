e426ef61f7fb86e8c8864cc45e76e873ccdb25a7
Viktor1-lab <putin975@yandex.ru>
2024-07-24 14:01:12 +0300

    fixed naming

---
+++ homework/TumanovViktor/hw_28/hw_28_selenium.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+
+# driver.set_window_size(1536, 750)
+driver.get('https://www.qa-practice.com/elements/input/simple')
+text_string = driver.find_element(By.ID, 'id_text_string')
+text_string.send_keys('qa')
+text_string.submit()
+sleep(1)
+
+result_text = driver.find_element(By.ID, 'result-text')
+assert result_text.text == 'qa'
+print('Текст который вы писали: ', result_text.text)
+sleep(2)
+
+dropdown_trigger = driver.find_element(By.ID, 'req_header')
+dropdown_trigger.click()
+sleep(4)
+
+click_concat_link = driver.find_element(By.PARTIAL_LINK_TEXT, 'Contact')
+click_concat_link.click()
+sleep(3)
+
+driver.close()
