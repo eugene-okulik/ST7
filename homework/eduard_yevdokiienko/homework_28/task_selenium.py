f719270613e92e900737c22585f47e2819150414
Eduard Yevdokiienko <eduard.evdokienko@gmail.com>
2024-07-24 13:22:44 +0100

    28_fixed

--- homework/eduard_yevdokiienko/homework_28/task_selenium.py
+++ homework/eduard_yevdokiienko/homework_28/task_selenium.py
@@ -8,21 +8,21 @@ driver.maximize_window()
 
 
 driver.get('https://www.qa-practice.com/elements/input/simple')
-search_input = driver.find_element(By.XPATH, "//input[@id='id_text_string']")
-search_input.send_keys('error')
-search_input.submit()
-search_element = driver.find_element(By.XPATH, "//p[@id='result-text']")
-assert search_element.text == 'error'
-print('Searched text: ', search_element.text)
+text_string = driver.find_element(By.XPATH, "//input[@id='id_text_string']")
+text_string.send_keys('error')
+text_string.submit()
+find_element = driver.find_element(By.XPATH, "//p[@id='result-text']")
+assert find_element.text == 'error'
+print('Searched text: ', find_element.text)
 sleep(2)
 
 buttons = driver.find_element(By.XPATH, "//a[contains(text(),'Buttons')]")
 buttons.click()
 button_click = driver.find_element(By.XPATH, "//input[@id='submit-id-submit']")
 button_click.click()
-submitted_element = driver.find_element(By.XPATH, "//p[@id='result-text']")
-assert submitted_element.text == 'Submitted'
-print('Searched text: ', submitted_element.text)
+result_text = driver.find_element(By.XPATH, "//p[@id='result-text']")
+assert result_text.text == 'Submitted'
+print('Searched text: ', result_text.text)
 sleep(2)
 
 
