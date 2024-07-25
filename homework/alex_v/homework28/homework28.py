bf3e195dfdc66c9a46ddf4eba1744d91cbbaf82f
Alex Verteyko <cracowaqa@gmail.com>
2024-07-25 19:32:58 +0300

    hometask 28

--- homework/alex_v/homework28/homework28.py
+++ homework/alex_v/homework28/homework28.py
@@ -2,19 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.maximize_window()
 
-expected_text = "java_rules"
-driver.get('https://www.qa-practice.com/elements/input/simple')
-search_input = driver.find_element(By.XPATH, "//input[@type='text']")
-search_input.click()
-search_input.send_keys(expected_text)
-driver.implicitly_wait(3)
-search_input.submit()
-driver.implicitly_wait(1)
 
-result_pop_up_text = driver.find_element(By.XPATH, "//input[@type='text']").text
-print(result_pop_up_text)
-
-driver.close()
-driver.quit()
+def test_ui_framework():
+    expected_text = "java_rules"
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    search_input = driver.find_element(By.XPATH, "//input[@type='text']")
+    search_input.click()
+    search_input.send_keys(expected_text)
+    driver.implicitly_wait(3)
+    search_input.submit()
+    driver.implicitly_wait(1)
+    result_pop_up_text = driver.find_element(By.XPATH, "//input[@type='text']").text
+    assert result_pop_up_text == expected_text
+    driver.close()
+    driver.quit()
