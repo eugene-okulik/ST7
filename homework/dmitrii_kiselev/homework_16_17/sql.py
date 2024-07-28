825feecdbfc24f63a4ef28bb80786cfb9e3fd83e
Dmitrii Kiselev <didroi@yandex.ru>
2024-07-28 13:52:35 +0200

    last_hope

--- homework/dmitrii_kiselev/homework_16_17/sql.py
+++ homework/dmitrii_kiselev/homework_16_17/sql.py
@@ -11,44 +11,51 @@ with mysql.connect(
 
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES ('st7', '01.04.2024', NULL)")
-    group_id = cursor.lastrowid
-
-    query = "INSERT INTO students (name, second_name, group_id) VALUES ('Dmitrii', 'Kiselev', %s)"
-    cursor.execute(query, (group_id,))
+    student_name, student_surname = 'Dmitry', 'Kiseljov'
+    query = "INSERT INTO students (name, second_name) VALUES (%s, %s)"
+    data = (student_name, student_surname)
+    cursor.execute(query, data)
     student_id_var = cursor.lastrowid
 
     query = 'INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)'
     cursor.executemany(
-        query, [('Информатика для чайников', student_id_var), ('Погружение в базы данных', student_id_var),
-                ('Гвидо ван Россум. Детство. Отрочество. Юность', student_id_var)]
+        query, [('Информатика для чайников. Издание 2', student_id_var),
+                ('Погружение в базы данных. Издание 2', student_id_var),
+                ('Гвидо ван Россум. Детство. Отрочество. Юность. Издание 2', student_id_var)]
     )
 
-    quary = 'INSERT INTO subjects (title) VALUES (%s)'
+    cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES ('st7', '01.04.2024', NULL)")
+    group_id = cursor.lastrowid
+
+    query = "UPDATE students SET group_id = %s WHERE name = %s AND second_name = %s"
+    data = (group_id, student_name, student_surname)
+    cursor.execute(query, data)
+
+    query = 'INSERT INTO subjects (title) VALUES (%s)'
     subject_ids = []
     for item in [['Automation testing on Python'], ['Automation testing on Java']]:
-        cursor.execute(quary, item)
+        cursor.execute(query, item)
         subject_ids.append(cursor.lastrowid)
 
     lesson_ids = []
-    quary = 'INSERT INTO lessons (title, subject_id) VALUES (%s, %s)'
+    query = 'INSERT INTO lessons (title, subject_id) VALUES (%s, %s)'
     lessons = ['python basics', 'java basics', 'advanced python', 'advanced java']
     for i in range(4):
-        cursor.execute(quary, (lessons[i], subject_ids[i % 2]))
+        cursor.execute(query, (lessons[i], subject_ids[i % 2]))
         lesson_ids.append(cursor.lastrowid)
 
-    quary = 'INSERT INTO marks (value, lesson_id, student_id) VALUES (%s, %s, %s)'
+    query = 'INSERT INTO marks (value, lesson_id, student_id) VALUES (%s, %s, %s)'
     marks_dict = {'python basics': 5, 'java basics': 3, 'advanced python': 4, 'advanced java': 2}
     for i in range(4):
-        cursor.execute(quary, [marks_dict[lessons[i]], lesson_ids[i], student_id_var])
+        cursor.execute(query, [marks_dict[lessons[i]], lesson_ids[i], student_id_var])
 
-    quary = "SELECT * FROM marks WHERE student_id = %s"
-    cursor.execute(quary, (student_id_var, ))
+    query = "SELECT * FROM marks WHERE student_id = %s"
+    cursor.execute(query, (student_id_var, ))
     marks_data = cursor.fetchall()
     print(marks_data)
 
-    quary = "SELECT * FROM books WHERE taken_by_student_id = %s"
-    cursor.execute(quary, (student_id_var, ))
+    query = "SELECT * FROM books WHERE taken_by_student_id = %s"
+    cursor.execute(query, (student_id_var, ))
     books_data = cursor.fetchall()
     print(books_data)
 
@@ -64,9 +71,10 @@ with mysql.connect(
     ON m.lesson_id = l.id
     JOIN subjects s2
     ON l.subject_id = s2.id
-    WHERE s.id = 80
+    WHERE s.id = %s
     '''
-    cursor.execute(query)
+    data = (student_id_var,)
+    cursor.execute(query, data)
     full_data = cursor.fetchall()
     print(full_data)
 
