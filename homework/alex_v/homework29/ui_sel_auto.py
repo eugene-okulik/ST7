ffcd9ce8862e61ab553bb4562c7f624c604bddc3
Alex Verteyko <cracowaqa@gmail.com>
2024-07-29 19:57:04 +0300

    UI selenium task 29

--- homework/alex_v/homework29/ui_sel_auto.py
+++ homework/alex_v/homework29/ui_sel_auto.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC, wait
 from selenium.webdriver.support.wait import WebDriverWait
 
 options = webdriver.FirefoxOptions()
-options.add_argument('start-maximized')
 driver = webdriver.Firefox(options=options)
+driver.set_window_size(1920, 1080)
 driver.get("https://demoqa.com/automation-practice-form")
 driver.implicitly_wait(5)
 
@@ -23,12 +23,19 @@ mobile_number.send_keys('1234567890')
 date_of_birth = driver.find_element(By.ID, 'dateOfBirthInput')
 date_of_birth.click()
 wait.WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, 'react-datepicker__month')))
-# date_of_birth.send_keys(Keys.CONTROL + "a")
-# date_of_birth.send_keys('01012000')
-# date_of_birth.send_keys(Keys.ENTER)
+date_of_birth.send_keys(Keys.CONTROL + "a")
+date_of_birth.send_keys('01012000')
+date_of_birth.send_keys(Keys.ENTER)
 subjects = driver.find_element(By.ID, 'subjectsInput')
-subjects.send_keys('English')
-subjects.send_keys(Keys.ENTER)
+driver.execute_script("arguments[0].scrollIntoView();", subjects)
+subjects.click()
+subjects.send_keys('en')
+english = wait.WebDriverWait(driver, 5).until(
+    EC.visibility_of_element_located((By.ID, 'react-select-2-option-0')))
+english.click()
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//div[text()='English']")))
+wait.WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//label[@for="hobbies-checkbox-1"]')))
 hobbies = driver.find_element(By.XPATH, '//label[@for="hobbies-checkbox-1"]')
 hobbies.click()
 address = driver.find_element(By.ID, 'currentAddress')
@@ -39,7 +46,11 @@ state.send_keys(Keys.ENTER)
 city = driver.find_element(By.ID, 'react-select-4-input')
 city.send_keys('Delhi')
 city.send_keys(Keys.ENTER)
-submit_button = driver.find_element(By.ID, 'submit')
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+submit_button = driver.find_element(By.XPATH, '//*[@id="submit"]')
 submit_button.click()
-wait.WebDriverWait(driver, 5).until(EC.visibility_of_element_located(By.ID, 'example-modal-sizes-title-lg'))
+info = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.XPATH, '//*[@class="table table-dark table-striped table-bordered table-hover"]'))).text
+print(info)
 driver.quit()
