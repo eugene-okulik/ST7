2663e6e0f6f2d2573c21e1ec9a9c6d42ad94d3de
Viktor1-lab <putin975@yandex.ru>
2024-07-31 12:00:31 +0300

    fixed task 1

--- homework/TumanovViktor/hw_30/hw_selenium_30.py
+++ homework/TumanovViktor/hw_30/hw_selenium_30.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import pytest
 
 
@@ -8,22 +12,21 @@ def driver():
     driver = webdriver.Firefox()
     driver.maximize_window()
     yield driver
-    # driver.quit()
+    driver.quit()
 
 
 def test_task1(driver):
     driver.get('https://www.qa-practice.com/elements/select/single_select')
-    select_choose = driver.find_element(By.ID, 'id_choose_language')
+    select_choose = driver.find_element(By.XPATH, '//select[@id="id_choose_language"]')
     driver.implicitly_wait(3)
-    select_choose.click()
-    select_choose.find_element(By.XPATH, '//*[@value="1"]').click()
-
-    click_button_submit = driver.find_element(By.ID, 'submit-id-submit')
-    click_button_submit.click()
+    dropdown = Select(select_choose)
+    dropdown.select_by_visible_text('Ruby')
+    # select_choose.find_element(By.XPATH, '//*[@value="1"]').click()
+    driver.find_element(By.ID, 'submit-id-submit').click()
 
     result_text = driver.find_element(By. ID, 'result-text')
     # print(result_text.text)
-    assert result_text.text == 'Python'
+    assert result_text.text == 'Ruby'
 
 
 def test_task2(driver):
