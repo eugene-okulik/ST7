3d80fef397c8c5a14e989dcfd0f913d1c8362e57
Kate Hushcha <katerinahushcha@gmail.com>
2024-08-01 08:32:48 -0400

    updated

--- homework/kate_hushcha/hw_selenium_basic#3/test_select.py
+++ homework/kate_hushcha/hw_selenium_basic#3/test_select.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
 import pytest
 
 
@@ -17,8 +18,8 @@ def test_select_language(driver):
     driver.get('https://www.qa-practice.com/elements/select/single_select')
     select_input = driver.find_element(By.CSS_SELECTOR, '#id_choose_language')
     select_input.click()
-    language_select = driver.find_element(By.XPATH, '//*[contains (text(), "Ruby")]')
-    language_select.click()
+    language_select = Select(select_input)
+    language_select.select_by_visible_text('Ruby')
     submit_button = driver.find_element(By.CSS_SELECTOR, '#submit-id-submit')
     submit_button.click()
     result_button = driver.find_element(By.ID, 'result')
