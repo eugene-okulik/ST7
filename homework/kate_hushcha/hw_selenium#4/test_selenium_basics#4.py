1220bb82c135378cb02e4daa47f15a950d916efc
Kate Hushcha <katerinahushcha@gmail.com>
2024-08-07 10:26:44 -0400

    kate_hw_selenium#4_completed

--- homework/kate_hushcha/hw_selenium#4/test_selenium_basics#4.py
+++ homework/kate_hushcha/hw_selenium#4/test_selenium_basics#4.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
-from time import sleep
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as ec
