7a588c3223a7c555af489e647865a7e5ddaa71dd
Viktor1-lab <putin975@yandex.ru>
2024-08-12 19:17:21 +0300

    tuman 33 done

---
+++ homework/TumanovViktor/hw_33_playwright/homeworck_33.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page
+import time
+
+
+def test_form(page: Page):
+    page.goto('https://the-internet.herokuapp.com/')
+    authentication = page.get_by_role('link', name='Form Authentication')
+    authentication.click()
+    time.sleep(1)
+    user_name = page.get_by_role('textbox', name='username')
+    user_name.fill('vitek')
+    user_name = page.get_by_role('textbox', name='password')
+    user_name.fill('123456zZ!')
+    login_button = page.get_by_role('button', name='Login')
+    login_button.click()
