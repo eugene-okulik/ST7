f77de1e88f1ae316aea99f5e76df502c6b24e1f7
Viktor1-lab <putin975@yandex.ru>
2024-08-15 09:44:50 +0300

    tuman project done

---
+++ ui_tests_tumanov/pages/eco_friendly.py
@@ -0,0 +1,23 @@
+from ui_tests_tumanov.pages.base_page import Basepage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from ui_tests_tumanov.pages.locators import EcoFriendly as loc
+
+
+class FriendlyPage(Basepage):
+    page_url = '/collections/eco-friendly.html'
+
+    def card_price(self, text):
+        wait = WebDriverWait(self.driver, 2)
+        price = wait.until(ec.visibility_of_element_located(loc.price))
+        assert price.text == text
+
+    def check_price_in_card(self, text):
+        wait = WebDriverWait(self.driver, 5)
+        price_card = wait.until(ec.visibility_of_element_located(loc.price))
+        assert price_card.text == text
+
+    def check_title_in_card(self, text):
+        wait = WebDriverWait(self.driver, 3)
+        title_card = wait.until(ec.visibility_of_element_located(loc.card_title))
+        assert title_card.text == text
