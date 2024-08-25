f6f7ef57f9b544b9b4ebdd33383abe129c005a15
Dmitrii Kiselev <didroi@yandex.ru>
2024-08-25 14:52:51 +0200

    dmitrii_apple_plawright_done

---
+++ homework/dmitrii_kiselev/homework_36_playwright_basics/test_playwright_4.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import Page, expect, Route, BrowserContext
+import pytest
+import re
+
+
+@pytest.fixture()
+def page(context: BrowserContext) -> Page:
+    page = context.new_page()
+    return page
+
+
+def test_1_alert(page):
+    
+    def change_response(route: Route):
+        response = route.fetch()
+        body = response.json()
+        body['body']['digitalMat'][0]['familyTypes'][0]['productName'] = 'яблокофон 15 про'
+        route.fulfill(response=response, json=body)
+    
+    page.route(re.compile('shop/api/digital-mat'), change_response)
+    page.goto('https://www.apple.com/shop/buy-iphone')
+    page.get_by_role('button', name='Take a closer look - iPhone 15 Pro & iPhone 15 Pro Max').click()
+    expect(page.locator('#rf-digitalmat-overlay-label-0').nth(0)).to_have_text('яблокофон 15 про')
+    page.get_by_role("button", name="Next", exact=True).click()
