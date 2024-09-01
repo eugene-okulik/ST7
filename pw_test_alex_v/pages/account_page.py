9ab5b9475845fef0092884d42ddd5fd32affaba6
Alex Verteyko <cracowaqa@gmail.com>
2024-09-01 10:55:55 +0300

    pw pages creation

--- pw_test_alex_v/pages/account_page.py
+++ pw_test_alex_v/pages/account_page.py
@@ -4,6 +4,5 @@ from pw_test_alex_v.pages.base_page import BasePage
 
 class Account(BasePage):
 
-    def successful_registration_message_displayed_is(self, message_text: str) -> bool:
-        success_msg = self.page.locator('.messages').first.inner_text()
-        return message_text.lower() in success_msg.lower()
+    def successful_registration_message_displayed_is(self, message_text: str):
+        expect(self.page.locator('.messages')).to_contain_text(message_text)
