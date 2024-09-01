3b1012931be926cc2a8b53440adf88225573e294
Alex Verteyko <cracowaqa@gmail.com>
2024-09-01 14:38:11 +0300

    remove spaces

--- pw_test_alex_v/pages/customer_login.py
+++ pw_test_alex_v/pages/customer_login.py
@@ -7,7 +7,6 @@ class CustomerLoginPage(BasePage):
     page_url = '/customer/account/login/'
 
     def invalid_login_notification_message_displayed_is(self, expected_message: str) -> bool:
-        # Locate the alert div using the correct Playwright locator method
         notification_locator = self.page.locator('//div[@role="alert"]')
         notification_locator.first.wait_for(state="visible", timeout=10000)
         notification_message = notification_locator.first.inner_text().strip()
